2489ccb0d25656fbf53a352fb2fb561db324d492
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-04-01 23:06:07 +0530

    solved

--- algorithmic_toolbox/divide_and_conquer/inversions.cpp
+++ algorithmic_toolbox/divide_and_conquer/inversions.cpp
@@ -1,15 +1,9 @@
 #include <bits/stdc++.h>
 #define int long long
 #define all(v) v.begin(), v.end()
-#define input(v)      \
-    for (auto &i : v) \
-    cin >> i
-#define decl_read(v, n) \
-    ints v(n);          \
-    input(v)
-#define print(v)      \
-    for (auto &i : v) \
-    cout << i << " "
+#define input(v) for (auto &i : v) cin >> i
+#define decl_read(v, n) ints v(n); input(v)
+#define print(v) for (auto &i : v) cout << i << " "
 #define pb push_back
 using namespace std;
 typedef vector<int> ints;
@@ -33,7 +27,7 @@ ints merge(const ints &l, const ints &r, int &c) {
 ints ms(ints &v, int &c) {
     if (v.size() < 2)
         return v;
-    int m = v.size() / 2;
+    int m(v.size() / 2);
     ints l(v.begin(), v.begin() + m), r(v.begin() + m, v.end());
     return merge(ms(l, c), ms(r, c), c);
 }
