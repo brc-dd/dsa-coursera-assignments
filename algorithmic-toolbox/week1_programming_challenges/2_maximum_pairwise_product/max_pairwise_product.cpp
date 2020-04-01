e5b9c46ad20ad08314b1d8bd0deed78ff9752766
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-04-01 16:11:01 +0530

    solved

--- algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,20 +1,27 @@
+// Programmed by Divyansh Singh (brc-dd)
+
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
-    int n = numbers.size();
-
-    for (int first = 0; first < n; ++first) {
-        for (int second = first + 1; second < n; ++second) {
-            max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+int MaxPairwiseProduct(const std::vector<int> &numbers) {
+    int p1(0), p2(0), n1(0), n2(0);
+    for (auto i : numbers)
+        if (i >= 0) {
+            if (i > p1) {
+                p2 = p1;
+                p1 = i;
+            } else if (i > p2)
+                p2 = i;
+        } else {
+            if (i < n1) {
+                n2 = n1;
+                n1 = i;
+            } else if (i < n2)
+                n2 = i;
         }
-    }
-
-    return max_product;
-}
+    return std::max(p1 * p2, n1 * n2);
+} // O(n) time complexity ||| O(1) space complexity
 
 int main() {
     int n;
@@ -23,7 +30,6 @@ int main() {
     for (int i = 0; i < n; ++i) {
         std::cin >> numbers[i];
     }
-
-    std::cout << MaxPairwiseProduct(numbers); << "\n";
+    std::cout << MaxPairwiseProduct(numbers) << "\n";
     return 0;
 }
