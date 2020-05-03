279684ad53575a8854751b48d8fa668f8737406a
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-05-03 17:32:30 +0530

    solved

--- algorithmic_toolbox/dynamic_programming1/primitive_calculator.cpp
+++ algorithmic_toolbox/dynamic_programming1/primitive_calculator.cpp
@@ -1,31 +1,24 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
-using std::vector;
-
-vector<int> optimal_sequence(int n) {
-    std::vector<int> sequence;
-    while (n >= 1) {
-        sequence.push_back(n);
-        if (n % 3 == 0) {
-            n /= 3;
-        } else if (n % 2 == 0) {
-            n /= 2;
-        } else {
-            n = n - 1;
-        }
-    }
-    reverse(sequence.begin(), sequence.end());
-    return sequence;
-}
-
-int main() {
-    int n;
-    std::cin >> n;
-    vector<int> sequence = optimal_sequence(n);
-    std::cout << sequence.size() - 1 << std::endl;
-    for (size_t i = 0; i < sequence.size(); ++i) {
-        std::cout << sequence[i] << ' ';
-    }
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+#define pb push_back
+using namespace std;
+int32_t main() {
+	int n;
+	cin >> n;
+	int dp[n + 1] = {0};
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0) dp[i] = min(dp[i / 2] + 1, dp[i]);
+		if (i % 3 == 0) dp[i] = min(dp[i / 3] + 1, dp[i]);
+	}
+	cout << dp[n] << '\n';
+	int ptr = n;
+	vector<int> seq;
+	seq.pb(ptr);
+	while (ptr != 1)
+		seq.pb((ptr = (((ptr % 2) ? -1 : (ptr / 2)) == -1) ? ((((ptr % 3) ? -1 : (ptr / 3)) == -1) ? ((ptr - 1)) : (dp[(ptr - 1)] < dp[((ptr % 3) ? -1 : (ptr / 3))] ? (ptr - 1) : ((ptr % 3) ? -1 : (ptr / 3)))) : ((((ptr % 3) ? -1 : (ptr / 3)) == -1) ? (dp[(ptr - 1)] < dp[((ptr % 2) ? -1 : (ptr / 2))] ? (ptr - 1) : ((ptr % 2) ? -1 : (ptr / 2))) : (dp[(ptr - 1)] < dp[((ptr % 2) ? -1 : (ptr / 2))] ? (dp[(ptr - 1)] < dp[((ptr % 3) ? -1 : (ptr / 3))] ? (ptr - 1) : ((ptr % 3) ? -1 : (ptr / 3))) : (dp[((ptr % 2) ? -1 : (ptr / 2))] < dp[((ptr % 3) ? -1 : (ptr / 3))] ? ((ptr % 2) ? -1 : (ptr / 2)) : ((ptr % 3) ? -1 : (ptr / 3)))))));
+	reverse(all(seq));
+	for (auto &i : seq)
+		cout << i << ' ';
+	return 0;
 }
