4c1177867f397da41b086381d97d2f03fe50ff53
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-05-03 18:08:04 +0530

    solved

--- algorithmic_toolbox/dynamic_programming1/edit_distance.cpp
+++ algorithmic_toolbox/dynamic_programming1/edit_distance.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
-#include <string>
-
-using std::string;
-
-int edit_distance(const string &str1, const string &str2) {
-    //write your code here
-    return 0;
-}
-
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    string str1;
-    string str2;
-    std::cin >> str1 >> str2;
-    std::cout << edit_distance(str1, str2) << std::endl;
-    return 0;
+	string a, b;
+	cin >> a >> b;
+	int l1(a.size()), l2(b.size());
+	int dp[2][l1 + 1] = {0};
+	for (int i(0); i <= l1; ++i)
+		dp[0][i] = i;
+	for (int i(1); i <= l2; ++i)
+		for (int j(0); j <= l1; ++j)
+			if (!j)
+				dp[i & 1][j] = i;
+			else if (a[j - 1] == b[i - 1])
+				dp[i & 1][j] = dp[(i - 1) & 1][j - 1];
+			else
+				dp[i & 1][j] = 1 + min(dp[(i - 1) & 1][j], min(dp[i & 1][j - 1], dp[(i - 1) & 1][j - 1]));
+	cout << dp[l2 & 1][l1];
+	return 0;
 }
