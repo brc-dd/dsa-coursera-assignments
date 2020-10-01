2dff05f70e1eb12ef70185bea5926a52f5f37727
pavnesh2020 <72187021+pavnesh2020@users.noreply.github.com>
2020-10-01 13:08:52 +0530

    Solved Week 5 Assignments (#1)
    * solved
    
    * solved lcs3

--- algorithmic_toolbox/dynamic_programming1/lcs2.cpp
+++ algorithmic_toolbox/dynamic_programming1/lcs2.cpp
@@ -1,11 +1,21 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using std::vector;
 
 int lcs2(vector<int> &a, vector<int> &b) {
-    //write your code here
-    return std::min(std::min(a.size(), b.size()), c.size());
+    int n = a.size(), m = b.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+    for (int i = 0; i <= n; ++i)
+        for (int j = 0; j <= m; ++j)
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if (a[i - 1] == b[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+    return dp[n][m];
 }
 
 int main() {
