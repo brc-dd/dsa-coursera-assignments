e90177bc3523d0ed3c4e9e299c69238623922d52
Pavnesh Chaturvedi <72187021+pavnesh2020@users.noreply.github.com>
2020-10-02 09:10:45 -0900

    cleaned code - Week 6

--- algorithmic_toolbox/dynamic_programming2/partition3.cpp
+++ algorithmic_toolbox/dynamic_programming2/partition3.cpp
@@ -1,49 +1,50 @@
 #include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <numeric>
 #include <vector>
+using namespace std;
 
-using std::vector;
+#define int long long
+#define all(v) v.begin(), v.end()
+#define iit istream_iterator<int>(cin)
 
-bool helper(vector<int> &A, int subset[3], vector<bool> &taken, int subsetSum,
+bool helper(vector<int> &v, int subset[3], vector<bool> &taken, int subsetSum,
             int l, int r) {
-    if (subset[l] == subsetSum) {
-        if (l == 1)
-            return true;
-        return helper(A, subset, taken, subsetSum, l + 1, A.size() - 1);
+  if (subset[l] == subsetSum) {
+    if (l == 1)
+      return true;
+    return helper(v, subset, taken, subsetSum, l + 1, v.size() - 1);
+  }
+  for (int i = r; i; --i)
+    if (not taken[i] and subset[l] + v[i] <= subsetSum) {
+      taken[i] = true;
+      subset[l] += v[i];
+      if (helper(v, subset, taken, subsetSum, l, i - 1))
+        return true;
+      taken[i] = false;
+      subset[l] -= v[i];
     }
-    for (int i = r; i; --i)
-        if (not taken[i] and subset[l] + A[i] <= subsetSum) {
-            taken[i] = true;
-            subset[l] += A[i];
-            if (helper(A, subset, taken, subsetSum, l, i - 1))
-                return true;
-            taken[i] = false;
-            subset[l] -= A[i];
-        }
+  return false;
+}
+
+int solve(vector<int> &v) {
+  int n = v.size();
+  if (n < 3)
     return false;
+  int sum = accumulate(v.begin(), v.end(), 0LL);
+  if (sum % 3 != 0)
+    return false;
+  int subsetSum = sum / 3;
+  int subset[3] = {0};
+  vector<bool> taken(n);
+  subset[0] = v[n - 1];
+  taken[0] = true;
+  return helper(v, subset, taken, subsetSum, 0, n - 1);
 }
 
-bool partition3(vector<int> &A) {
-    int n = A.size();
-    if (n < 3)
-        return false;
-    int sum = std::accumulate(A.begin(), A.end(), 0);
-    if (sum % 3 != 0)
-        return false;
-    int subsetSum = sum / 3;
-    int subset[3] = {0};
-    vector<bool> taken(n);
-    subset[0] = A[n - 1];
-    taken[0]  = true;
-    return helper(A, subset, taken, subsetSum, 0, n - 1);
-}
-
-int main() {
-    int n;
-    std::cin >> n;
-    vector<int> A(n);
-    for (size_t i = 0; i < A.size(); ++i) {
-        std::cin >> A[i];
-    }
-    std::cout << partition3(A) << '\n';
+signed main() {
+  vector<int> v(*iit);
+  copy_n(iit, v.size(), v.begin());
+  cout << solve(v);
 }
